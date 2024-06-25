8a8babdbaf7078ff040d9112b965e1e264e5cd74
rendykml <rd24rk@gmail>
2024-06-26 03:12:38 +0700

    nambahin file nyetak dan rubah history agar terbagi tiap tanggal

--- poin_of_sales/history.php
+++ poin_of_sales/history.php
@@ -5,20 +5,18 @@ session_start();
 
 include 'auth_admincheck.php';
 
-$view = $dbconnect->query("SELECT * FROM transaksi");
+$view = $dbconnect->query("SELECT * FROM transaksi ORDER BY DATE(tanggal_waktu), id_transaksi");
 
 if (!$view) {
     die("Error menjalankan query: " . $dbconnect->error);
 }
 ?>
 
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>List Users</title>
+    <title>Riwayat Transaksi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
@@ -26,7 +24,6 @@ if (!$view) {
     <script src="style/admin.js" ></script>
     <link rel="stylesheet" href="style/admin-flex.css">
     <link rel="stylesheet" href="style/admin.css">  
-    </style>
 </head>
 
 <body>
@@ -42,13 +39,13 @@ if (!$view) {
                 <li><a class="text-decoration-none" href="index.php" id="dashboard-link"><i class="fa-solid fa-house"></i> Dashboard</a></li>
                 <li><a class="text-decoration-none" href="user.php" id="user-link"><i class="fa-solid fa-users"></i> Users</a></li>
                 <li><a class="text-decoration-none" href="produk.php" id="produk-link"><i class="fa-solid fa-list-check"></i> Produk</a></li>
-                <li><a class="text-decoration-none" href="history.php" id="history-link"><i class="fa-solid fa-box"></i> transaksi</a></li>
+                <li><a class="text-decoration-none" href="history.php" id="history-link"><i class="fa-solid fa-box"></i> Transaksi</a></li>
             </ul>
         </div>
 
         <div class="content">
             <nav class="navbar navbar-expand-lg bg-light " id="top_nav">
-                <div class="container-fluid pt-2 ps-4 " >
+                <div class="container-fluid pt-2 ps-4">
                     <a class="navbar-brand text-black" href="index.php"><h4><i>Point Of Sales</i></h4></a>
                     <div class="collapse navbar-collapse justify-content-end">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -67,8 +64,8 @@ if (!$view) {
                     </div>
                 </div>
             </nav>
-        <!-- New container for welcome message and actions -->
-        <div class="container ">
+            
+            <div class="container">
                 <div class="row">
                     <div class="col-12">
                         <div class="p-1 py-4 bg-light rounded">
@@ -80,40 +77,56 @@ if (!$view) {
                 </div>
             </div>
 
-            <div class="container mt-3 table-responsive">
-                <table class="table align-middle">
-                    <thead class="table-light">
-                        <tr>
-                            <th class="ps-3">ID Transaksi</th>
-                            <th>Waktu Transaksi</th>
-                            <th>Nomor Transaksi</th>
-                            <th>Total Harga</th>
-                            <th>Nama Kasir</th>
-                            <th>Jumlah Bayar</th>
-                            <th>Kembalian</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($row = $view->fetch_array()) { ?>
-                            <tr>
-                                <th class="ps-5"><?= $row['id_transaksi'] ?></th>
-                                <td><?= $row['tanggal_waktu'] ?></td>
-                                <td><?= $row['nomor_transaksi'] ?></td>
-                                <td><?= $row['total'] ?></td>
-                                <td><?= $row['nama_user'] ?></td>
-                                <td><?= $row['bayar'] ?></td>
-                                <td><?= $row['kembali'] ?></td>
-                                <!-- <td>
-                                    Hapus
-                                    <a class="btn btn-danger" href="produk_hapus.php?id=<?= $row['id_produk'] ?>">Hapus</a>
-                                </td> -->
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+            <div class="container mt-3">
+                <?php
+                $current_date = "";
+                while ($row = $view->fetch_array()) {
+                    $date = date('Y-m-d', strtotime($row['tanggal_waktu']));
+                    if ($date != $current_date) {
+                        if ($current_date != "") {
+                            echo "</tbody></table>
+                                  <div class='text-end mb-1'>
+                                      <a href='history_hari.php?date=$current_date' class='btn btn-primary'>Cetak Data</a>
+                                  </div>
+                                  </div>";
+                        }
+                        $current_date = $date;
+                        echo "<div class='table-responsive'>";
+                        echo "<h2>" . date('d M Y', strtotime($current_date)) . "</h2>";
+                        echo "<table class='table align-middle'>";
+                        echo "<thead class='table-light'>
+                                <tr>
+                                    <th class='ps-3'>ID Transaksi</th>
+                                    <th>Waktu Transaksi</th>
+                                    <th>Nomor Transaksi</th>
+                                    <th>Total Harga</th>
+                                    <th>Nama Kasir</th>
+                                    <th>Jumlah Bayar</th>
+                                    <th>Kembalian</th>
+                                </tr>
+                              </thead>
+                              <tbody>";
+                    }
+                    echo "<tr>
+                            <th class='ps-5'>{$row['id_transaksi']}</th>
+                            <td>{$row['tanggal_waktu']}</td>
+                            <td>{$row['nomor_transaksi']}</td>
+                            <td>{$row['total']}</td>
+                            <td>{$row['nama_user']}</td>
+                            <td>{$row['bayar']}</td>
+                            <td>{$row['kembali']}</td>
+                          </tr>";
+                }
+                if ($current_date != "") {
+                    echo "</tbody></table>
+                          <div class='text-end mb-2'>
+                              <a href='history_hari.php?date=$current_date' class='btn btn-primary'>Cetak Data</a>
+                          </div>
+                          </div>";
+                }
+                ?>
             </div>
         </div>
-            
     </div>
 </body>
 
---
+++ poin_of_sales/history_hari.php
@@ -0,0 +1,67 @@
+<?php
+include 'config.php';
+
+session_start();
+
+include 'auth_admincheck.php';
+
+if (!isset($_GET['date'])) {
+    die("Tanggal tidak tersedia.");
+}
+
+$date = $_GET['date'];
+
+$view = $dbconnect->query("SELECT * FROM transaksi WHERE DATE(tanggal_waktu) = '$date' ORDER BY id_transaksi");
+
+if (!$view) {
+    die("Error menjalankan query: " . $dbconnect->error);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Print Riwayat Transaksi - <?= date('d M Y', strtotime($date)) ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style/admin.css">
+</head>
+
+<body onload="window.print()">
+    <div class="container mt-3">
+        <div class="table-responsive">
+            <h2>Riwayat Transaksi - <?= date('d M Y', strtotime($date)) ?></h2>
+            <table class="table align-middle">
+                <thead class="table-light">
+                    <tr>
+                        <th class="ps-3">ID Transaksi</th>
+                        <th>Waktu Transaksi</th>
+                        <th>Nomor Transaksi</th>
+                        <th>Total Harga</th>
+                        <th>Nama Kasir</th>
+                        <th>Jumlah Bayar</th>
+                        <th>Kembalian</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $view->fetch_array()) { ?>
+                        <tr>
+                            <th class="ps-5"><?= $row['id_transaksi'] ?></th>
+                            <td><?= $row['tanggal_waktu'] ?></td>
+                            <td><?= $row['nomor_transaksi'] ?></td>
+                            <td><?= $row['total'] ?></td>
+                            <td><?= $row['nama_user'] ?></td>
+                            <td><?= $row['bayar'] ?></td>
+                            <td><?= $row['kembali'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
